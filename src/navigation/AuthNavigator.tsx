18a6bcb09b49eeb80aa6ed6ce9ad2c8850fb74db
carlosviano <carlitosviano@gmail.com>
2025-03-01 16:23:07 +0100

    wip

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,2 +1,4 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
