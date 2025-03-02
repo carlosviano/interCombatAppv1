dbee59fc224b762dc11186b7b3e7d232c8820a14
carlosviano <carlitosviano@gmail.com>
2025-03-02 12:03:56 +0100

    utilice mi tema en estas pantallas. Cambie los componentes por los mios personalizados.

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,28 +1,45 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, Text, useTheme } from "react-native-paper";
 import { useAuthStore } from "../../state/authStore";
+import { StyledTextInput } from "@/src/components/StyledInput";
+import ButtonContained from "../../components/ButtonContained";
 
 const LoginScreen = ({ navigation }: any) => {
+  const { colors } = useTheme();
   const login = useAuthStore((state) => state.login);
 
   return (
-    <View style={styles.container}>
-      <Text variant="titleLarge">Iniciar Sesión</Text>
-      <TextInput label="Email" style={styles.input} />
-      <TextInput label="Contraseña" secureTextEntry style={styles.input} />
-      <Button mode="contained" onPress={login} style={styles.button}>
-        Entrar
-      </Button>
-      <Button onPress={() => navigation.navigate("PreLoginScreen")}>
-        Registrarse
-      </Button>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <Text variant="titleLarge" style={{ color: "white" }}>
+        Iniciar Sesión
+      </Text>
+      <StyledTextInput label="Email" style={styles.input} />
+      <StyledTextInput
+        label="Contraseña"
+        secureTextEntry
+        style={styles.input}
+      />
+      <ButtonContained
+        buttonText="Entrar"
+        onPress={login}
+        style={styles.button}
+      />
+      <ButtonContained
+        buttonText="Registrarme"
+        onPress={() => navigation.navigate("PreLoginScreen")}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", padding: 20 },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
   input: { marginBottom: 10 },
   button: { marginVertical: 10 },
 });
