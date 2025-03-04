3c74695556f7ec720703f398d4c6afefe905dd9d
carlosviano <carlitosviano@gmail.com>
2025-03-04 23:45:55 +0100

    Made changes to properly align boxes and text using more divs instead of manually using margins

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,47 +1,32 @@
-import ClickableBox from "@/src/components/ClickableBox";
-import { MainStackParamList } from "@/src/navigation/MainNavigator";
-import { useNavigation, NavigationProp } from "@react-navigation/native";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import ClickableBox from "@/src/components/ClickableBox"; // tu componente con imagen + texto debajo
 import { useTheme } from "react-native-paper";
 
-const HomeScreen = () => {
-  const { colors } = useTheme();
+export default function HomeScreen() {
   const userName = "Carlos";
   const cajasMock = ["Profile", "Eventos", "Calendario", "Noticias"];
-  const navigation = useNavigation<NavigationProp<MainStackParamList>>();
+  const { colors } = useTheme();
 
   return (
     <SafeAreaView
       style={[styles.container, { backgroundColor: colors.background }]}
     >
       <View style={styles.content}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.title}>Bienvenido, {userName}</Text>
-          <Text style={styles.subtitle}>
-            Quedan 10 días para tu próxima pelea
-          </Text>
-        </View>
+        <Text style={styles.title}>Bienvenido, {userName}</Text>
+        <Text style={styles.subtitle}>
+          Quedan 10 días para tu próxima pelea
+        </Text>
 
         <View style={styles.boxContainer}>
-          {cajasMock.map((item) =>
-            item === "Profile" ? (
-              <ClickableBox
-                text={item}
-                key={item}
-                onPress={() => navigation.navigate("ProfileFlow")}
-              />
-            ) : (
-              <ClickableBox text={item} key={item} />
-            )
-          )}
+          {cajasMock.map((item) => (
+            <ClickableBox text={item} key={item} />
+          ))}
         </View>
       </View>
     </SafeAreaView>
   );
-};
-
-export default HomeScreen;
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -49,27 +34,27 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
+    width: "90%",
+    alignSelf: "center",
     justifyContent: "center",
-    paddingHorizontal: 16,
-  },
-  headerContainer: {
-    marginBottom: 20,
-    paddingHorizontal: 32,
-    alignItems: "flex-start",
+    paddingVertical: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: "600",
     color: "#FFF",
+    marginBottom: 4,
+    marginLeft: 16,
   },
   subtitle: {
     fontSize: 16,
     color: "#FFF",
-    marginTop: 4,
+    marginBottom: 20,
+    marginLeft: 16,
   },
   boxContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
-    justifyContent: "center",
+    justifyContent: "space-between",
   },
 });
